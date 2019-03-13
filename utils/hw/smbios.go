ac1fd7063ef5dabe0fce725362804667f9c8a5f9
casoul li <lijingwei9060@qq.com>
2019-03-13 12:08:06 +0800

    add smbios info

--- utils/hw/smbios.go
+++ utils/hw/smbios.go
@@ -1,84 +1,64 @@
 package hw
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/lijingwei9060/go-smbios/smbios"
+	"github.com/lijingwei9060/scout/lib/common"
 )
 
-func getBIOSInformation() {
-	rc, ep, err := smbios.Stream()
-	if err != nil {
-		log.Fatalf("failed to open stream: %v", err)
-	}
-	// Be sure to close the stream!
-	defer rc.Close()
+// FetchHWInfo 通过smbios获取服务器硬件信息
+func FetchHWInfo() common.MapStr {
+	hw := common.MapStr{}
+	bios := smbios.GetSMBIOS()
 
-	// Decode SMBIOS structures from the stream.
-	d := smbios.NewDecoder(rc)
-	ss, err := d.Decode()
-	if err != nil {
-		log.Fatalf("failed to decode structures: %v", err)
+	if bios == nil {
+		return hw
 	}
 
-	major, minor, rev := ep.Version()
-	fmt.Printf("SMBIOS %d.%d.%d\n", major, minor, rev)
+	hw.Put("smbios.major", bios.Major)
+	hw.Put("smbios.minor", bios.Minor)
 
-	for _, s := range ss {
-		// Only look at memory devices.
-		// if s.Header.Type == 17 {
-		// 	out, err := smbios.ParseMemoryDevice(s)
-		// 	if err != nil {
-		// 		fmt.Print(err)
-		// 	}
-		// 	str, _ := json.Marshal(out)
-		// 	fmt.Print(string(str))
-		// }
-
-		// Code based on: https://www.dmtf.org/sites/default/files/standards/documents/DSP0134_3.1.1.pdf.
-
-		if s.Header.Type == 0 {
-			out, err := smbios.ParseBIOSInformation(s)
-			if err != nil {
-				fmt.Print(err)
-			}
-			str, _ := json.Marshal(out)
-			fmt.Print(string(str))
-		}
-		if s.Header.Type == 1 {
-			out, err := smbios.ParseSystemInformation(s)
-			if err != nil {
-				fmt.Print(err)
-			}
-			str, _ := json.Marshal(out)
-			fmt.Print(string(str))
-		}
-		if s.Header.Type == 2 {
-			out, err := smbios.ParseBaseboardInformation(s)
-			if err != nil {
-				fmt.Print(err)
-			}
-			str, _ := json.Marshal(out)
-			fmt.Print(string(str))
-		}
-		if s.Header.Type == 3 {
-			out, err := smbios.ParseSystemEnclosure(s)
-			if err != nil {
-				fmt.Print(err)
-			}
-			str, _ := json.Marshal(out)
-			fmt.Print(string(str))
-		}
-		if s.Header.Type == 4 {
-			out, err := smbios.ParseProcessorInformation(s)
-			if err != nil {
-				fmt.Print(err)
-			}
-			str, _ := json.Marshal(out)
-			fmt.Print(string(str))
-		}
-
+	// type 0
+	hw.Put("bios.vendor", bios.BIOSInformation.Vendor)
+	hw.Put("bios.version", bios.BIOSInformation.BIOSVersion)
+	hw.Put("bios.releasedate", bios.BIOSInformation.BIOSReleaseDate)
+	// type 1
+	hw.Put("system.manufacturer", bios.SystemInformation.Manufacturer)
+	hw.Put("system.productname", bios.SystemInformation.ProductName)
+	hw.Put("system.serialnumber", bios.SystemInformation.SerialNumber)
+	hw.Put("system.version", bios.SystemInformation.Version)
+	hw.Put("system.uuid", bios.SystemInformation.UUID)
+	// type 2
+	for n, b := range bios.BaseboardInformations {
+		hw.Put(fmt.Sprintf("baseboard.%d.manufacturer", n), b.Manufacturer)
+		hw.Put(fmt.Sprintf("baseboard.%d.productname", n), b.ProductName)
+		hw.Put(fmt.Sprintf("baseboard.%d.version", n), b.Version)
+		hw.Put(fmt.Sprintf("baseboard.%d.serialnumber", n), b.SerialNumber)
+		hw.Put(fmt.Sprintf("baseboard.%d.boardtype", n), b.BoardType)
 	}
+
+	// type 3
+	for n, s := range bios.SystemEnclosures {
+		hw.Put(fmt.Sprintf("chassis.%d.manufactor", n), s.Manufacturer)
+		hw.Put(fmt.Sprintf("chassis.%d.type", n), s.Type)
+		hw.Put(fmt.Sprintf("chassis.%d.version", n), s.Version)
+		hw.Put(fmt.Sprintf("chassis.%d.serialnumber", n), s.SerialNumber)
+	}
+
+	// type 4
+	for n, p := range bios.ProcessorInformations {
+		hw.Put(fmt.Sprintf("processor.%d.version", n), p.ProcessorVersion)
+		hw.Put(fmt.Sprintf("processor.%d.cores", n), p.CoreCount)
+		hw.Put(fmt.Sprintf("processor.%d.threads", n), p.ThreadCount)
+	}
+
+	// type 17
+	for n, m := range bios.MemoryDevices {
+		hw.Put(fmt.Sprintf("memory.%d.manufacturer", n), m.Manufacturer)
+		hw.Put(fmt.Sprintf("memory.%d.size", n), m.Size)
+		hw.Put(fmt.Sprintf("memory.%d.serialnumber", n), m.SerialNumber)
+		hw.Put(fmt.Sprintf("memory.%d.speed", n), m.Speed)
+	}
+	return hw
 }
